14a005cfef0751886f60c821cd0808c03b0770dd
tejasvaidya94 <tejasvaidya123580@gmail.com>
2026-04-22 17:32:15 +0530

    updated the approach: recursion but with partitioning logic.

--- leetcode_131.java
+++ leetcode_131.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 class leetcode_131 {
-    public boolean isPalindrom(StringBuilder sb) {
+    public boolean isPalindrom(String s) {
         int l = 0;
-        int r = sb.length() - 1;
+        int r = s.length() - 1;
         while (l <= r) {
-            if (sb.charAt(l) != sb.charAt(r)) {
+            if (s.charAt(l) != s.charAt(r)) {
                 return false;
             }
             l++;
@@ -15,30 +15,24 @@ class leetcode_131 {
         return true;
     }
 
-    public void recursion(String s, int i, List<List<String>> ans, List<String> list, StringBuilder sb) {
-        if (i == s.length()) {
-            ans.add(new ArrayList<>(list));
+    public void recursion(String s, List<List<String>> ans, List<String> partitions) {
+        if (s.length() == 0) {
+            ans.add(new ArrayList<>(partitions));
             return;
         }
-        sb.append(s.charAt(i));
-        if (isPalindrom(sb)) {
-            list.add(new String(sb));
-            // empty sb here.
-            recursion(s, i + 1, ans, list, new StringBuilder());
-            list.remove(list.size() - 1);
+        for (int i = 0; i < s.length(); i++) {
+            if (isPalindrom(s.substring(0, i + 1))) {
+                partitions.add(s.substring(0, i + 1));
+                recursion(s.substring(i + 1), ans, partitions);
+                partitions.remove(partitions.size() - 1);
+            }
         }
-        if (i + 1 == s.length()) {
-            list = new ArrayList<>();
-            return;
-        }
-        recursion(s, i + 1, ans, list, sb);
     }
 
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
-        List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        recursion(s, 0, ans, list, sb);
+        List<String> partitions = new ArrayList<>();
+        recursion(s, ans, partitions);
         return ans;
     }
 }
