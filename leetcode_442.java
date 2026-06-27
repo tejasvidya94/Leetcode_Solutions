9144bb72fb9109b3bc141c81da4fe47b5eeec74e
tejasvaidya94 <tejasvaidya123580@gmail.com>
2026-06-27 14:15:52 +0530

    Implement solution for leetcode problem 442: Find All Duplicates in an Array

---
+++ leetcode_442.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class leetcode_442 {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int idx = Math.abs(nums[i]) - 1;
+            if (nums[idx] < 0) {
+                ans.add(idx + 1);
+            } else {
+                nums[idx] = -nums[idx];
+            }
+        }
+        return ans;
+    }
+}
